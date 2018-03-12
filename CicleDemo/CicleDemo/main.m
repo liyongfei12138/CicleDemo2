c02915ae6c27528ef543a8c564d002c6c1e65a20
liyongfei12138 <miap972712779@qq.com>
2018-03-12 15:16:56 +0800

    1

---
+++ CicleDemo/CicleDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CicleDemo
+//
+//  Created by liyongfei on 2018/3/12.
+//  Copyright © 2018年 liyongfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
